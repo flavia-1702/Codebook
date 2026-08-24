a3e15e358d2d0155f6ded367c05e77be841e09fe
flavia-1702 <167451562+flavia-1702@users.noreply.github.com>
2026-08-24 15:09:08 -0300

    Create festival_de_cinema.cpp

---
+++ CSES/festival_de_cinema.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Festival de cinema
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+ 
+    vector<pair<int, int>> filmes(n);
+ 
+    for(int i = 0; i < n; i++){
+        int a, b; cin >> a >> b;
+        filmes[i] = {b, a};
+    }
+ 
+    sort(filmes.begin(), filmes.end());
+ 
+    int cont = 0, fim_atual = 0;
+ 
+    for(auto& f : filmes){
+        if(f.second >= fim_atual){
+            cont++;
+            fim_atual = f.first;
+        }
+    }
+ 
+    cout << cont << "\n";
+
+    return 0;
+}
