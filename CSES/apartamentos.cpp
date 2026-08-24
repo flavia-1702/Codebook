48fb3103253f787a425d40155a5a199c19e11d6f
flavia-1702 <167451562+flavia-1702@users.noreply.github.com>
2026-08-24 14:58:14 -0300

    Update apartamentos.cpp

--- CSES/apartamentos.cpp
+++ CSES/apartamentos.cpp
@@ -1,1 +1,41 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    vector<ll> pessoas(n);
+    vector<ll> apartamentos(m);
+
+    for(ll i = 0; i < n; i++) cin >> pessoas[i];
+    for(ll i = 0; i < m; i++) cin >> apartamentos[i];
+
+    sort(pessoas.begin(), pessoas.end());
+    sort(apartamentos.begin(), apartamentos.end());
+
+    ll resp = 0, i = 0, j = 0;
+    while(i < n && j < m){
+        if(pessoas[i] - k > apartamentos[j])
+            j++;
+        else{
+            if(pessoas[i] + k < apartamentos[j])
+                i++;
+            else if(pessoas[i] - k <= apartamentos[j] && pessoas[i] + k >= apartamentos[j]){
+                resp++;
+                i++;
+                j++;
+            }
+        }
+    }
+
+    cout << resp << "\n";
+
+    return 0;
+}
